cfce116e3cb2af506330ba85df5455b9d3bfdb73
Arel <chindyaurellat@gmail.com>
2025-04-30 17:19:04 +0700

    Dapat melihat kegiatan yang sudah ditambahkan di aplikasi

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1>Daftar Kegiatan</h1>
+    <ul v-if="activities.length">
+      <li v-for="(activity, index) in activities" :key="index">
+        {{ activity }}
+      </li>
+    </ul>
+    <p v-else>Tidak ada kegiatan yang tersedia.</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const activities = ref([
+  'Belajar Vue.js',
+  'Mengerjakan tugas',
+  'Olahraga pagi',
+  'Membaca buku'
+])
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  max-width: 600px;
+  margin: auto;
+  padding: 2rem;
+  font-family: sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+h1 {
+  color: #333;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+li {
+  margin-bottom: 0.5rem;
 }
 </style>
